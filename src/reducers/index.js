d707c158f28f8d2e31092e95c9b0ac1a3b6a0d8c
Rajesh Kolluri <kollurirajesh25@gmsil.com>
2021-11-25 16:05:28 +0530

    react redux bolgpost

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import {combineReducers} from "redux";
+import postReducer from "./postReducer";
+import usersReducer from "./userReducer";
+
+export default combineReducers({posts:postReducer,users:usersReducer});
